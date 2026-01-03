ade67c32a2be88c27571e4bc100d80f3fa394522
nofyso <1052470899@qq.com>
2026-01-03 20:12:14 +0800

    fix: fixed wrong term select

--- lib/presentation/util/class_time_util.dart
+++ lib/presentation/util/class_time_util.dart
@@ -8,13 +8,31 @@ class ClassTimeUtil {
     return (-difference.inDays / 7.0).floor().clamp(0, 19);
   }
 
-  static TermData? selectCurrentTermData(DateTime time, List<TermData> termDataList) {
+  static TermData? selectCurrentTermData(
+    DateTime time,
+    List<TermData> termDataList,
+  ) {
     final year = time.year;
     final month = time.month;
-    final semester = DateTime.march.to(DateTime.september).contains(month) ? "2" : "1";
-    final academicYear = semester == "1" ? "$year-${year + 1}" : "${year - 1}-$year";
+    final (semester, academicYear) = switch (month) {
+      final x when DateTime.january <= x && x < DateTime.march => (
+        "1",
+        "${year - 1}-$year",
+      ),
+      final x when DateTime.march <= x && x < DateTime.september => (
+        "2",
+        "${year - 1}-$year",
+      ),
+      final x when DateTime.september <= x && x <= DateTime.december => (
+        "1",
+        "$year-${year + 1}",
+      ),
+      _ => throw AssertionError(""),
+    };
     return termDataList
-        .filter((it) => it.semester == semester && it.academicYear == academicYear)
+        .filter(
+          (it) => it.semester == semester && it.academicYear == academicYear,
+        )
         .firstOrNull();
   }
 }
